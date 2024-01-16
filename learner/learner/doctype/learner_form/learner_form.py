8e58526141b703454c187ad4bd295db5aac2cdd8
Ajamat <ajamat.suvaidyam@gmail.com>
2024-01-16 16:52:26 +0530

    create web-form

---
+++ learner/learner/doctype/learner_form/learner_form.py
@@ -0,0 +1,11 @@
+# Copyright (c) 2024, Rahul Kumar and contributors
+# For license information, please see license.txt
+
+# import frappe
+# from frappe.website.website_generator import WebsiteGenerator
+from frappe.model.document import Document
+
+
+
+class LearnerForm(Document):
+	pass

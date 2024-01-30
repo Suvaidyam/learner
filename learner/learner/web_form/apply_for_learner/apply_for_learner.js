9f05ebb2db80f15d26846adf6f303c14acc0a12b
bittu9835 <bk2671111@gmail.com>
2024-01-30 18:27:55 +0530

    add

--- learner/learner/web_form/apply_for_learner/apply_for_learner.js
+++ learner/learner/web_form/apply_for_learner/apply_for_learner.js
@@ -1,24 +1,37 @@
+async function filterDistrict(state) {
+    var myurl = '/api/method/frappe.desk.search.search_link';
+    return new Promise(function (resolve, reject) {
+        $.ajax({
+            type: 'GET',
+            url: myurl,
+            data: {
+                doctype: 'District',
+                filters: JSON.stringify({
+                    'state': state
+                }),
+                txt: ''
+            },
+            success:async function (result) {
+                var options = await result.message;
+                resolve(options);
+            },
+            error: function (err) {
+                console.error(err.responseText);
+                reject(err);
+            }
+        });
+    });
+}
+
 frappe.ready(function () {
-	// bind events here
-	frappe.web_form.on("state", () => {
-		let state = frappe.web_form.get_value('state');
-		console.log(state);
-
-		if (state) {
-			return {
-				filters: {
-					"district": ["=", state]
-				}
-			};
-		} else {
-			return {
-				filters: {
-					"district": ["=", "Please select a state"]
-				}
-			};
-		}
+	frappe.web_form.on('state', async (field, value) => {
+		let ops = await filterDistrict(value);
+		console.log('asdadad',ops)
+        console.log(frappe.web_form)
+		frappe.web_form.set_df_property("district", "is_web_form", ops);
+        // frappe.web_form.fields
 	});
+});
 
-})
 
 document.querySelector("div.page_content").style.maxWidth = "100%";

80614917f027ebd87d587ad5bdc443d9b5875d06
Samar Raj <samar.suvaidyam@gmail.com>
2024-02-24 20:43:23 +0000

    added more questions

---
+++ learner/learner/doctype/career_options/career_options.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Rahul Kumar and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Career Options", {
+// 	refresh(frm) {
+
+// 	},
+// });

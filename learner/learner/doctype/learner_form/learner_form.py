1878f048ff6908ec531ac5825786b19899082d3d
rahulsuvaidyam <rahul.suvaidyam@gmail.com>
2024-02-26 18:48:50 +0530

    add email

--- learner/learner/doctype/learner_form/learner_form.py
+++ learner/learner/doctype/learner_form/learner_form.py
@@ -3,7 +3,35 @@
 
 # import frappe
 from frappe.model.document import Document
-
+from learner.services.send_mail import MSG
 
 class LearnerForm(Document):
-	pass
+	def on_update(self):
+		if self.application_status =='Accepted':
+		
+			MSG.send_email(self.full_name,self.email,self.application_status)
+		elif self.application_status == 'Rejected': 
+			MSG.send_email(self.full_name,self.email,self.application_status)
+		elif self.application_status == 'Open': 
+			data = {
+				"full_name":self.full_name,
+				"email":self.email,
+				"phone_zwil":self.phone_zwil,
+				"block":self.block,
+				"panchayat":self.panchayat,
+				"village":self.village,
+				"passing_year_10th":self.passing_year_10th,
+				"total_obtain_marks":self.total_obtain_marks,
+				"obtain_marks_in_science":self.obtain_marks_in_science,
+				"obtain_marks_in_maths":self.obtain_marks_in_maths,
+				"result_not_yet_declared":self.result_not_yet_declared,
+				"passing_year_12th":self.passing_year_12th,
+				"stream":self.stream,
+				"main_subject":self.main_subject,
+				"obtain_marks_in_maths12":self.obtain_marks_in_maths12,
+				"total_obtain_marks12":self.total_obtain_marks12,
+				"career_option":self.career_option,
+			}
+			MSG.send_email(self.full_name,'abhishek.suvaidyam@gmail.com',self.application_status,data)
+
+	
---
+++ learner/services/send_mail.py
@@ -0,0 +1,74 @@
+import smtplib
+import frappe
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+class MSG:
+    def send_email(name,user_eamil,subjects,data=None):
+        sender_email = 'rkrahul00011@gmail.com'
+        sender_password = frappe.conf.get('mail_psw')
+        receiver_email = user_eamil
+        subject = subjects
+        accepted = f"""
+        <html>
+          <body class="bg-light">
+            <div style="background:#F8F9FA" class="p-2">
+                <div style="border-radius:10px;background:white;padding:10px;">
+                    <div style="border-radius:8px;background:#efefef;padding:10px;font-size:15px;">
+                    <p style="font-size:15px">Hello {name} 👋,</p>
+                    <p style="font-size:15px">I hope you are doing well. 
+                        We are excited to inform you that after a thorough review of the 
+                        applications, you have been selected for the Suvaidyam Learner Program.</p>
+                     Congratulations 😍 on this achievement! Your enthusiasm for learning and your 
+                     commitment to personal and professional development stood out among the applicants,
+                       and we believe that you will contribute significantly to the program.
+                </div>
+             </div>
+            </div>
+          </body>
+        </html>
+        """
+        rejected = f"""
+        <html>
+          <body class="bg-light">
+            <div style="background:#F8F9FA" class="p-2">
+                <div style="border-radius:10px;background:white;padding:10px;">
+                    <div style="border-radius:8px;background:#efefef;padding:10px;font-size:15px;">
+                    <p style="font-size:15px">Hello {name} 👋,</p>
+                    <p style="font-size:15px">I trust this message finds you well.
+                      We appreciate the time and effort you invested in applying for the Suvaidyam Learner Program.</p>
+                     <p>After careful consideration and review of all applications, we regret to inform you that your application was not selected 😔 for the program. While we received numerous impressive applications, the selection process was highly competitive.</p>
+                     <p>We commend your interest in the program and encourage you to continue pursuing your passion for learning and professional development. The decision-making process was challenging, and unfortunately, we were unable to accommodate all qualified candidates.</p>
+                     <p>We genuinely appreciate your interest in our program and wish you the best in your future endeavors. Thank you for your understanding.</p>
+                </div>
+             </div>
+            </div>
+          </body>
+        </html>
+        """
+        admin = f"""
+        <html>
+          <body class="bg-light">
+            <div style="background:#F8F9FA" class="p-2">
+                <div style="border-radius:10px;background:white;padding:10px;">
+                    <p style="font-size:15px">Hello Abhishek Singh 👋,</p>
+                    <p style="font-weight:600;font-size:15px">New Students</p>
+                    <div style="border-radius:8px;background:#efefef;padding:10px;font-size:15px;">
+                     {data}
+                </div>
+             </div>
+            </div>
+          </body>
+        </html>
+        """
+        
+        body = 'accepted' if subject == 'Accepted' else (rejected if subject == 'Rejected' else admin)  
+        message = MIMEMultipart()
+        message['From'] = 'Suvaidyam <{}>'.format(sender_email)
+        message['To'] = receiver_email
+        message['Subject'] = subject
+        message.attach(MIMEText(body, 'html'))
+
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465) as server:
+            server.login(sender_email, sender_password)
+            server.send_message(message)
